adfcf912704b464c5d0cf5e2686d11903b72e768
Justin-Chua <justin.chua@ucalgary.ca>
2024-03-29 14:09:42 -0600

    Updated README.md

--- back_end/parking_app/parking_app/settings.py
+++ back_end/parking_app/parking_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_n(r6ocv6tu)ta$rr897cw=8yv)6ipexudz73vj$p7tek8@3vk'
+SECRET_KEY = 'django-insecure-^g^ad)r%mtcfhy*uge%8n861bg0z-cp#wmi^y^(4_=4p!!b@a='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'parking_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'testdb',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
