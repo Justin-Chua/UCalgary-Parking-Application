68c892b5105464986aa2f61c72673699faaf43d8
Justin-Chua <justin.chua@ucalgary.ca>
2024-03-28 19:03:45 -0600

    Added folder/file templates for frontend

---
+++ front_end/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <button>This is a header</button>
+        </div>
+    );
+}
+
+export default Header;
---
+++ front_end/src/pages/Home.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+function Home() {
+    return (
+        <div>
+            <Header />
+            <h1>Hello World!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        </div>
+    );
+}
+
+export default Home;
