187e8614509aaaddf7114a3f44731319a6db4605
Justin-Chua <justin.chua@ucalgary.ca>
2024-04-01 22:23:32 -0600

    partial completion of database schema in models.py

--- README.md
+++ README.md
@@ -5,26 +5,25 @@
 - Chiehhan Yang (30110238)
 ## Project Setup
 ### Back End
-0.1. Make sure that the latest version of MySQL is installed
-0.2. Make sure that the latest version of XAMPP is installed to host the database
+0. Make sure that the latest version of MySQL and XAMPP is installed
 1. After installing, navigate to the back_end folder using `back_end`
 2. Install the necessary dependency using the following command:
-- 2.1. MySQLClient: `pip install mysqlclient`
+    - 2.1. MySQLClient: `pip install mysqlclient`
 3. Ensure that models are updated using the command `python manage.py migrate`
 4. Start the backend using the command `python manage.py runserver`
 5. To view the backend, you have two options:
-- 5.1. Navigate to `127.0.0.1:8000/api/` in the browser to add values manually or view them
-- 5.2. Click on 'Admin' beside MySQL in XAMPP, and view the database on `localhost/phpmyadmin/`
+    - 5.1. Navigate to `127.0.0.1:8000/api/` in the browser to add values manually or view them
+    - 5.2. Click on 'Admin' beside MySQL in XAMPP, and view the database on `localhost/phpmyadmin/`
 
 ### Front End
 0. Make sure that the latest version of Node.js is installed on your computer
 1. After installing Node.js, navigate to the front_end folder using `cd front_end`
 2. Install all the necessary dependencies using the following commands:
-- 2.1. React: `npm install react`
-- 2.2. React Routers: `npm install react-router-dom@latest`
-- 2.3. Bootstrap: `npm install bootstrap react-bootstrap`
-- 2.4. Bootstrap Icons: `npm install react-bootstrap-icons`
-- 2.5. Leaflet: `npm install leaflet react-leaflet`
+    - 2.1. React: `npm install react`
+    - 2.2. React Routers: `npm install react-router-dom@latest`
+    - 2.3. Bootstrap: `npm install bootstrap react-bootstrap`
+    - 2.4. Bootstrap Icons: `npm install react-bootstrap-icons`
+    - 2.5. Leaflet: `npm install leaflet react-leaflet`
  3. To start the React application, use the command `npm start`
  4. The application will automatically open in a new browser window, on `localhost:3000`
 
--- back_end/parking/models.py
+++ back_end/parking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Todo(models.Model):
@@ -8,3 +9,134 @@ class Todo(models.Model):
     def __str__(self):
         """A string representation of the model."""
         return self.title
+    
+class UniversityMember(models.Model):
+    ucid = models.PositiveIntegerField(
+        primary_key=True,
+        validators=[MinLengthValidator(8), MaxLengthValidator(8)]
+    )
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_no = models.PositiveIntegerField(
+        validators=[MinLengthValidator(10), MaxLengthValidator(10)]
+    )
+
+class Client(models.Model):
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    # add plate number
+
+class ParkingAdmin(models.Model):
+    admin_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    
+class Ticket(models.Model):
+    ticket_no = models.AutoField(primary_key=True)
+    # add notification
+    # add payment
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    admin_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.SET_NULL, on_update=models.CASCADE
+    )
+    issue_date = models.DateField()
+    due_date = models.DateField()
+    offense = models.CharField(max_length=50)
+    amount_due = models.PositiveIntegerField()
+    paid = models.BooleanField(default=False)
+
+class Notification(models.Model):
+    notification_id = models.AutoField(primary_key=True)
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    title = models.CharField(max_length=50)
+    message = models.TextField(max_length=300)
+
+class Vehicle(models.Model):
+    plate_no = models.CharField(max_length=7)
+    make = models.CharField(max_length=20)
+    model = models.charField(max_length=100)
+    # add lot number
+
+class ParkingPermit(models.Model):
+    permit_no = models.AutoField(primary_key=True)
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    admin_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.SET_NULL, on_update=models.CASCADE
+    )
+    # add payment number
+    pp_issue_date = models.DateField()
+    pp_expiry_date = models.DateField()
+
+class Payment(models.Model):
+    payment_no = models.AutoField(primary_key=True)
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.CASCADE, on_update=models.CASCADE)
+    cc_holder = models.CharField(max_length=200)
+    cc_number = models.PositiveIntegerField(
+        validators=[MinLengthValidator(12), MaxLengthValidator(19)]
+    )
+    cvc = models.PositiveSmallIntegerField(
+        validators=[MinLengthValidator(3), MaxLengthValidator(3)]
+    )
+    cc_expiry_month = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)]
+    )
+    cc_expiry_year = models.PositiveSmallIntegerField(
+        validators=[MinLengthValidator(2), MaxLengthValidator(2)]
+    )
+
+class ParkingLot(models.Model):
+    lot_no = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(36)]
+    )
+    capacity = models.PositiveSmallIntegerField()
+    occupied_spaces = models.PositiveSmallIntegerField()
+
+class Reservation(models.Model):
+    lot_no = models.ForeignKey(
+        ParkingLot, to_field='lot_no', on_delete=models.CASCADE, on_update=models.CASCADE, primary_key=True
+    )
+    client_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.SET_NULL, on_update=models.CASCADE, primary_key=True
+    )
+    payment_no = models.ForeignKey(
+        Payment, to_field='payment_no', on_delete=models.CASCADE, on_update=models.CASCADE
+    )
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+class ParkingSpace(models.Model):
+    lot_no = models.ForeignKey(
+        ParkingLot, to_field='lot_no', on_delete=models.CASCADE, on_update=models.CASCADE, primary_key=True
+    )
+    zone = models.CharField(max_length=1)
+    stall_no = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(99)]
+    )
+
+class Patrols(models.Model):
+    lot_no = models.ForeignKey(
+        ParkingLot, to_field='lot_no', on_delete=models.CASCADE, on_update=models.CASCADE, primary_key=True
+    )
+    admin_ucid = models.ForeignKey(
+        UniversityMember, to_field='ucid', on_delete=models.SET_NULL, on_update=models.CASCADE, primary_key=True
+    )
+
+
+
+
+
+
+
+
+
