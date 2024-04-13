52da756d315b8291cf62a26b5a297ea767e459d2
Justin-Chua <justin.chua@ucalgary.ca>
2024-04-13 16:13:23 -0600

    backend for reservation complete

--- front_end/src/pages/Reservation.js
+++ front_end/src/pages/Reservation.js
@@ -1,49 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import * as Bootstrap from 'react-bootstrap';
 import * as Icons from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Reservation() {
+
+    const [userReservations, setUserReservations] = useState([]);
+
+    const fetchUserReservations = async () => {
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                console.error('Token not found');
+                return;
+            }
+            const response = await axios.get('http://127.0.0.1:8000/api/view-reservations/', {
+                headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+                }
+            });
+            setUserReservations(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUserReservations();
+    }, []);
+
     return (
         // Add backend for this later - currently hardcoded
-        <>
+        <>  
             <h1 id="page-header">Parking Reservations</h1>
-            <Bootstrap.Container className="card-container">
-                <Bootstrap.Card border="dark" className="d-flex">
-                    <Bootstrap.Card.Header className="d-flex bg-berry justify-content-between align-items-center text-white">
-                        <h1>Lot 10 - D12</h1>
-                        <Icons.CalendarCheckFill className="icon-size" />
-                    </Bootstrap.Card.Header>
-                    <Bootstrap.Card.Body className="text-center">
-                        <Bootstrap.Card.Text><strong>UCID: </strong>30098941</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Payment Number: </strong>8888888</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Date: </strong>3/30/2024</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Start Time: </strong>6:00PM</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>End Time: </strong>8:00PM</Bootstrap.Card.Text>
-                    </Bootstrap.Card.Body>
-                    <Bootstrap.Card.Footer className="bg-berry">
-                        <small className="text-white">Active</small>
-                    </Bootstrap.Card.Footer>
-                </Bootstrap.Card>
-            </Bootstrap.Container>
-            <Bootstrap.Container className="card-container">
-                <Bootstrap.Card border="dark" className="d-flex">
-                    <Bootstrap.Card.Header className="d-flex bg-berry justify-content-between align-items-center text-white">
-                        <h1>Lot 10 - D12</h1>
-                        <Icons.CalendarCheckFill className="icon-size" />
-                    </Bootstrap.Card.Header>
-                    <Bootstrap.Card.Body className="text-center">
-                        <Bootstrap.Card.Text><strong>UCID: </strong>30098941</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Payment Number: </strong>8888888</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Date: </strong>3/30/2024</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>Start Time: </strong>6:00PM</Bootstrap.Card.Text>
-                        <Bootstrap.Card.Text><strong>End Time: </strong>8:00PM</Bootstrap.Card.Text>
-                    </Bootstrap.Card.Body>
-                    <Bootstrap.Card.Footer className="bg-berry">
-                        <small className="text-white">Active</small>
-                    </Bootstrap.Card.Footer>
-                </Bootstrap.Card>
-            </Bootstrap.Container>
+            {userReservations.map(reservation => (
+                <Bootstrap.Container key={ reservation.lot_no } className="card-container">
+                    <Bootstrap.Card border="dark" className="d-flex">
+                        <Bootstrap.Card.Header className="d-flex bg-berry justify-content-between align-items-center text-white">
+                            <h1>{ reservation.lot_no }</h1>
+                            <Icons.CalendarCheckFill className="icon-size" />
+                        </Bootstrap.Card.Header>
+                        <Bootstrap.Card.Body className="text-center">
+                            <Bootstrap.Card.Text><strong>UCID: </strong>{ reservation.client_ucid }</Bootstrap.Card.Text>
+                            <Bootstrap.Card.Text><strong>Payment Number: </strong>{ reservation.payment_no }</Bootstrap.Card.Text>
+                            <Bootstrap.Card.Text><strong>Date: </strong>{ reservation.date }</Bootstrap.Card.Text>
+                            <Bootstrap.Card.Text><strong>Start Time: </strong>{ reservation.start_time }</Bootstrap.Card.Text>
+                            <Bootstrap.Card.Text><strong>End Time: </strong>{ reservation.end_time }</Bootstrap.Card.Text>
+                        </Bootstrap.Card.Body>
+                        <Bootstrap.Card.Footer className="bg-berry">
+                            <small className="text-white">Active</small>
+                        </Bootstrap.Card.Footer>
+                    </Bootstrap.Card>
+                </Bootstrap.Container>
+            ))}
     </>    
     );
 }
